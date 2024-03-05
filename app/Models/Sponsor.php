4061cc16c1f587a7648e4136a84b54c696e51a4f
joel-casado <joel.casado@inslapineda.cat>
2024-03-05 13:01:21 +0100

    Sponsors

--- app/Models/Sponsor.php
+++ app/Models/Sponsor.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Sponsor extends Model
 {
     use HasFactory;
+    protected $table = 'sponsors';
+    protected $primaryKey = 'CIF';
+    protected $fillable = [
+        'CIF',
+        'nom',
+        'logo',
+        'adreça',
+        'destacat'
+    ];
+
 }
