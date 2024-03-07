d8c6b68931e0eb4b0a198a486b179a602c35b9e4
joel-casado <joel.casado@inslapineda.cat>
2024-03-07 09:18:27 +0100

    Diseño sponsor

--- resources/views/admin/sponsors/tablaSponsors.blade.php
+++ resources/views/admin/sponsors/tablaSponsors.blade.php
@@ -1,17 +1,31 @@
 @include('layouts.adminHeader')
 
 <div class="container mt-4 d-flex flex-row">
-    <div class="row">
+<div class="flex-column bg-dark align-self-start justify-content-end vertical-buttons">
+    <div class="mb-30">
+        <div class="btn-group-vertical">
+            <a href="{{ route('carreras') }}" class="btn btn-link mb-2">Carreras</a>
+            <a href="{{ route('asseguradoras') }}" class="btn btn-link mb-2">Aseguradoras</a>
+            <a href="#" class="btn btn-link mb-2 d-none d-lg-block">Sponsors</a>
+        </div>
+    </div>
+</div>
+    <div class="row mr-5 carrerasTable">
         <div class="col">
-            <h2>Sponsors</h2>
-            <form method="GET" action="{{ route('addSponsor') }}">
-                <button id="btn-add" class="btn btn-primary rounded-circle"><i class="fas fa-plus"></i></button>
-            </form>
-            <div style="max-height: 800px; overflow-y: auto;">
+        <div class="d-flex justify-content-between titulo">
+            <h2>Aseguradoras</h2> 
+                <form method="GET" action="{{ route('addSponsor') }}">
+                    @csrf
+                    <button id="btn-add" type="submit" class="btn btn-primary btn-lg rounded-circle">
+                        <i class="fas fa-plus"></i>
+                    </button>
+                </form>
+            </div>
+            <div class="tbodyCont" style="max-height: 700px; overflow-y: auto;">
                 <table class="table">
                     <thead>
                         <tr>
-                            <th class="text-center">Logo</th>
+                        <th class="text-center">Logo</th>
                             <th class="text-center">Nombre</th>
                             <th class="text-center">CIF</th>
                             <th class="text-center">Dirección</th>
@@ -19,7 +33,6 @@
                             <th colspan="2" class="text-center">Acciones</th>
                         </tr>
                     </thead>
-                
                     <tbody>
                         @foreach($sponsors as $sponsor)
                         <tr>
@@ -33,17 +46,11 @@
                                     @csrf
                                     <button type="submit" class="btn btn-info btn-editar">Editar</button>
                                 </form>
-                                <form method="GET" action="{{ route('editarSponsor', $sponsor->CIF) }}">
-                                    @csrf
-                                    <button type="submit" class="btn btn-info btn-editar">Patrocinar</button>
-                                </form>
                             </td>
                         </tr>
                         
                         @endforeach
                     </tbody>
-                    
-                
                 </table>
             </div>
         </div> 
