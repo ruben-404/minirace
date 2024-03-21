f17a247d063f779e9b2b18e35df0f05310d1b62a
ruben-404 <ruben.jimenez0290@gmail.com>
2024-03-21 10:03:56 +0100

    aaa

--- resources/views/principal/paginas/infoCarreras.blade.php
+++ resources/views/principal/paginas/infoCarreras.blade.php
@@ -2,12 +2,12 @@
 
 <div class="container mt-5">
     <div class="row">
+        <div class="col-md-4">
+            <img src="{{ asset('storage/carrerasImages/' . $carrera->cartellPromoció) }}" alt="Cartel de la carrera" class="img-fluid">
+        </div>
         <div class="col-md-8 text-white">
             <h2>{{ $carrera->nom }}</h2>
             <p>{{ $carrera->descripció }}</p>
-        </div>
-        <div class="col-md-4">
-            <img src="{{ asset('storage/carrerasImages/' . $carrera->cartellPromoció) }}" alt="Cartel de la carrera" class="img-fluid">
         </div>
     </div>
 </div>
