7bf3d08d10898e753c649361e505f6a25b6fba56
joel-casado <joel.casado@inslapineda.cat>
2024-03-21 12:44:50 +0100

    cambio de titulo

--- resources/views/principal/paginas/home.blade.php
+++ resources/views/principal/paginas/home.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Bootstrap Carousel</title>
+    <title>MiniRace</title>
     <!--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
