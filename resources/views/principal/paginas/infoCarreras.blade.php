59a3c9d4644014ab906092a3a03a0f83d33fefb0
ruben-404 <ruben.jimenez0290@gmail.com>
2024-03-29 13:41:51 +0100

    aa

--- resources/views/principal/paginas/infoCarreras.blade.php
+++ resources/views/principal/paginas/infoCarreras.blade.php
@@ -8,11 +8,12 @@
             <img src="{{ asset('storage/carrerasImages/' . $carrera->cartellPromoció) }}" alt="Cartel de la carrera" class="img-fluid">
         </div>
         <div class="col-md-4 mx-auto descricioCont">
-            <p>{{ $carrera->descripció }}</p>
+            <!-- Oculto en dispositivos móviles -->
+            <p class="d-none d-md-block">{{ $carrera->descripció }}</p>
 
         </div>
         <div class="accordion mt-5" id="accordionExample">
-            <div class="accordion-item desplegableCont">
+            <div class="accordion-item desplegableCont text-white">
                 <h2 class="accordion-header" id="headingOne">
                     <button class="accordion-button collapsed desplegableInfo text-white" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
                         Mapa
@@ -20,7 +21,7 @@
                 </h2>
                 <div id="collapseOne" class="accordion-collapse collapse desplegableInfo" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                     <div class="accordion-body text-white d-flex justify-content-center align-items-center">
-                        <img src="{{ asset('storage/carrerasImages/' . $carrera->imatgeMapa) }}" alt="Mapa de la carrera" class="img-fluid">
+                        <img src="{{ asset('storage/carrerasImages/' . $carrera->imatgeMapa) }}" alt="Mapa de la carrera" class="img-fluid imgMapaInfo">
                     </div>
                 </div>
             </div>
@@ -45,14 +46,28 @@
                 </h2>
                 <div id="collapseThree" class="accordion-collapse collapse desplegableInfo" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                     <div class="accordion-body text-white">
-                        <p>{{ $carrera->hora }}</p>
-                        <p>{{ $carrera->data }}</p>
+                        <p>Hora: {{ $carrera->hora }}</p>
+                        <p>Fecha: {{ $carrera->data }}</p>
 
                     </div>
                 </div>
             </div>
+            <!-- Visible solo en dispositivos móviles -->
+            <div class="accordion-item desplegableCont d-block d-md-none">
+                <h2 class="accordion-header" id="headingDescription">
+                    <button class="accordion-button collapsed desplegableInfo text-white" type="button" data-bs-toggle="collapse" data-bs-target="#collapseDescription" aria-expanded="false" aria-controls="collapseDescription">
+                        Descripción
+                    </button>
+                </h2>
+                <div id="collapseDescription" class="accordion-collapse collapse desplegableInfo" aria-labelledby="headingDescription" data-bs-parent="#accordionExample">
+                    <div class="accordion-body text-white">
+                        <p>{{ $carrera->descripció }}</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        
-        
+
+
+    </div>
 </div>
 @include('principal.componentes.footer')
